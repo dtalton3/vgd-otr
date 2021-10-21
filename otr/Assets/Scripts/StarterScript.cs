d7884d498d89a75a570c134f8efdde3aef3ed32b
dtalton3 <55326674+dtalton3@users.noreply.github.com>
2021-10-20 21:15:03 -0400

    edit script

--- otr/Assets/Scripts/StarterScript.cs
+++ otr/Assets/Scripts/StarterScript.cs
@@ -7,7 +7,7 @@ public class StarterScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("SlattBidness World.");
     }
 
     // Update is called once per frame
