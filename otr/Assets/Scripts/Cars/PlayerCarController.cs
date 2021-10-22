fb22f4081be9d4506aafef8474e063cf9dfd5749
dtalton3 <55326674+dtalton3@users.noreply.github.com>
2021-10-22 13:52:45 -0400

    car movement && camera follow

---
+++ otr/Assets/Scripts/Cars/PlayerCarController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currSteerAngle;
+    private float currBrakeForce; 
+    private bool isBraking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteeringAngle;
+
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        currBrakeForce = isBraking ? breakForce : 0f;
+        ApplyBrakes();
+    }
+
+    private void HandleSteering() 
+    {
+        currSteerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currSteerAngle;
+        frontRightWheelCollider.steerAngle = currSteerAngle;
+    }
+
+    private void ApplyBrakes() 
+    {
+        frontLeftWheelCollider.brakeTorque = currBrakeForce;
+        frontRightWheelCollider.brakeTorque = currBrakeForce;
+        rearLeftWheelCollider.brakeTorque = currBrakeForce;
+        rearRightWheelCollider.brakeTorque = currBrakeForce;
+    }
+
+    private void GetInput() 
+    {
+        horizontalInput = Input.GetAxisRaw(HORIZONTAL);
+        verticalInput = Input.GetAxisRaw(VERTICAL);
+        isBraking = Input.GetKeyDown(KeyCode.S);
+    }
+
+    private void UpdateWheels() 
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
